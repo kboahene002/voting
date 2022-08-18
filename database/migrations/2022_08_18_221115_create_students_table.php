598bbc8e0ee813267f388a9f1b76a434fbd4b2b0
kelvin B <kboahene002@st.ug.edu.gh>
2022-08-18 22:41:39 +0000

    added database migration

--- database/migrations/2022_08_18_221115_create_students_table.php
+++ database/migrations/2022_08_18_221115_create_students_table.php
@@ -13,13 +13,12 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('students', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('student_id')->unique;
+            $table->string("password");
+            $table->string("class");
+            $table->string("course");
             $table->softDeletes();
             $table->timestamps();
         });
@@ -32,6 +31,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('students');
     }
 };
--- database/migrations/2022_08_18_223556_create_votes_table.php
+++ database/migrations/2022_08_18_223556_create_votes_table.php
@@ -13,13 +13,11 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('votes', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->integer('category_id');
+            $table->integer('contestant_id');
+            $table->integer('student_id');
             $table->softDeletes();
             $table->timestamps();
         });
@@ -32,6 +30,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('votes');
     }
 };
